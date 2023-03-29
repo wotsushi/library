d8a0d6ca40971a0bb373f3b4d7a1a6b0c7bea426
wotsushi <wotsushi@gmail.com>
2023-03-29 22:31:32 +0900

    add test cases for template.hpp

--- template.test.cpp
+++ template.test.cpp
@@ -14,4 +14,54 @@ void solve() {
     rep1(i, 3) v.push_back(i);
     assert(v == vector<ll>({1, 2, 3}));
   }
+  {
+    vector<ll> v = {2, 3, 1};
+    sort(all(v));
+    assert(v == vector<ll>({1, 2, 3}));
+  }
+  {
+    vector1<ll> v(3);
+    v[1] = 1;
+    v[2] = 2;
+    v[3] = 3;
+    assert(v[1] == 1);
+    assert(v[2] == 2);
+    assert(v[3] == 3);
+  }
+  {
+    ll x = 42;
+    bool flag = chmax(x, 57);
+    assert(x == 57);
+    assert(flag == true);
+  }
+  {
+    ll x = 42;
+    bool flag = chmax(x, 17);
+    assert(x == 42);
+    assert(flag == false);
+  }
+  {
+    ll x = 42;
+    bool flag = chmax(x, 42);
+    assert(x == 42);
+    assert(flag == false);
+  }
+  {
+    ll x = 42;
+    bool flag = chmin(x, 17);
+    assert(x == 17);
+    assert(flag == true);
+  }
+  {
+    ll x = 42;
+    bool flag = chmin(x, 57);
+    assert(x == 42);
+    assert(flag == false);
+  }
+  {
+    ll x = 42;
+    bool flag = chmin(x, 42);
+    assert(x == 42);
+    assert(flag == false);
+  }
 }
